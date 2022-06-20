c299ec93b88ca58453d3b9b2d44e9f998264fbf6
rfkih <rifkihidayat23@yahoo.com>
2022-06-20 10:21:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+            14: '14px',
+          '5xl': '3rem',
+          '6xl': '4rem',
+          '7xl': '5rem',
+        },
+        zIndex:{
+          '100': '100',
+        },
+        fontWeight: {
+          hairline: 100,
+          'extra-light': 100,
+          thin: 200,
+          light: 300,
+          normal: 400,
+          medium: 500,
+          semibold: 600,
+          bold: 700,
+          extrabold: 800,
+          'extra-bold': 800,
+          'black': 900,
+        },
+        backgroundColor: {
+          'main-bg': '#54F85',
+          'secondary-bg': '#2A2E#3',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+        },
+      },
+    },
+    plugins: [],
+  };
